f015dffd74a35ac122672c877f611803fc2535dc
oerraoui <itsscarl2022@gmail.com>
2026-05-13 00:34:15 +0100

    set the window , build , canavas

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,24 @@ use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 
 fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let window = video_subsystem
+        .window("Smart_Road", 800, 800)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut canvas = window
+        .into_canvas()
+        .accelerated()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
     Ok(())
 }
