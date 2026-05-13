e050f199bd005fa4fe1d84b7fa526230f5e8ed5e
oerraoui <itsscarl2022@gmail.com>
2026-05-13 02:13:45 +0100

    1-[macro 3la structs ] [using structs] defining

--- src/vehicule.rs
+++ src/vehicule.rs
@@ -7,3 +7,36 @@ pub enum Direction {
     Left,
     Right,
 }
+
+#[derive(Clone, Copy)]
+pub struct Vehicule {
+    pub x: i32,
+    pub y: i32,
+    pub direction: Direction,
+    pub speed: i32,
+    pub velocity: f32,
+    pub distance: i32,
+    pub timer: Instant,
+    pub states: bool,
+    pub frame_count: u32,
+    pub angle: f64,
+    pub turning: bool,
+}
+
+impl Vehicule {
+    pub fn new(x: i32, y: i32, direction: Direction, angle: f64) -> Self {
+        Vehicule {
+            x,
+            y,
+            direction,
+            angle,
+            speed: 3,
+            distance: 0,
+            timer: Instant::now(),
+            states: true,
+            frame_count: 0,
+            turning: false,
+            velocity: 0.0,
+        }
+    }
+}
